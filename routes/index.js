accc21ec0cef1b18449751aa7936bdbc5f8a06ae
Unknown <baymax1298@gmail.com>
2019-02-28 16:26:59 +0530

    pre-final update 2

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,9 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET about page. */
-// router.get('/about-us', function(req, res, next) {
-//   res.render('about');
-// });
+router.get('/about-us', function(req, res, next) {
+  res.render('about');
+});
 
 /* GET about page. */
 router.get('/events', function(req, res, next) {
@@ -29,6 +29,11 @@ router.get('/tech-team', function(req, res, next) {
 /* GET coordinator page. */
 router.get('/event-coordinators', function(req, res, next) {
   res.render('coordinators');
+});
+
+/* GET contact page. */
+router.get('/event-register', function(req, res, next) {
+  res.render('register');
 });
 
 /* GET contact page. */
